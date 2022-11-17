ebb30289efd44c8b2f62a2919eb0d6f2e8675406
Gwendal Le Bris <gwen.coding@gmail.com>
2022-11-17 16:09:15 +0100

    Back end over

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# Bookmark Controller
+class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+
+  def new
+    @bookmark = Bookmark.new
+    @movies = Movie.all
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list_id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :lists, only: %i[index show new create]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
 
-# As a user, I can create a movie list #create / #new
-# As a user, I can see the details of a movie list #show
-
-# As a user, I can bookmark a movie inside a movie list
 # As a user, I can destroy a bookmark
