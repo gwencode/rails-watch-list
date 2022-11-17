d375862ba6ec861d1756cbcf86005a054b6b29c8
Gwendal Le Bris <gwen.coding@gmail.com>
2022-11-17 13:12:51 +0100

    list controller 4 ations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# frozen_string_literal: true
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+Rails.application.routes.draw do
+  resources :lists, only: %i[index show new create]
 end
+
+# As a user, I can create a movie list #create / #new
+# As a user, I can see the details of a movie list #show
+
+# As a user, I can bookmark a movie inside a movie list
+# As a user, I can destroy a bookmark
---
+++ db/migrate/20221117111257_create_movies.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Create Movies
+class CreateMovies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :overview
+      t.text :poster_url
+      t.float :rating
+
+      t.timestamps
+    end
+  end
+end
