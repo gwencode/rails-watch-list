566f6b7e89e42dc502e630292592a3df86936088
Gwendal Le Bris <gwen.coding@gmail.com>
2022-11-18 12:05:34 +0100

    Small changes

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,7 +12,7 @@ class ListsController < ApplicationController
     @list = List.new(list_params)
 
     if @list.save
-      redirect_to list_path(@list)
+      redirect_to root_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: "lists#index"
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
   end
