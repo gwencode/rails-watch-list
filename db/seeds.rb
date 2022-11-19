8d76b4343c00f84af5e7d2f6ee7664abe815a18a
Gwendal Le Bris <gwen.coding@gmail.com>
2022-11-19 19:10:57 +0100

    Update Seed with good poster_url

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ movies = JSON.parse(movies_serialized)
 movies["results"].each do |movie|
   title = movie["title"]
   overview = movie["overview"]
-  poster_url = movie["poster_path"]
+  poster_url = "https://image.tmdb.org/t/p/original/#{movie['poster_path']}"
   rating = movie["vote_average"]
   Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
 end
